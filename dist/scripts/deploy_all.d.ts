044a6b0cfc44b1976ca016aec3773b9a56f807d6
Maxx <donm51353@gmail.com>
2026-08-31 14:17:20 +0100

    Add dist/ back to gitignore

--- dist/scripts/deploy_all.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * deploy_all.ts
- *
- * Deploy to Creditcoin CC3 Testnet:
- * 1. VerifiedShare receipt token (ERC-721)
- * 2. OwnershipVerifier ASC (linked to receipt token)
- */
-import "dotenv/config";
